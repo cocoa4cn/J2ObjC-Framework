b5874cbea77de6bc7de7e1e11ec5427048c48439
whl <tingsi@msn.com>
2017-07-06 18:47:53 +0800

    从我的地址下载j2objc

--- Scripts/download.sh
+++ Scripts/download.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 set -ev
 
-j2objc_version=1.0.2
-sha1_checksum=6f2f45b323ef92cc8d1b13dd789aebd1ab87562f
+zipfile=j2objc-1.1
+
+# sha1_checksum=6f2f45b323ef92cc8d1b13dd789aebd1ab87562f
 
 echo "fetching j2objc dist"
-curl -OL https://github.com/cocoa4cn/j2objc/releases/download/${j2objc_version}/j2objc-${j2objc_version}.zip
-echo "${sha1_checksum}  j2objc-${j2objc_version}.zip" | shasum -c
-unzip -o -q j2objc-${j2objc_version}.zip
-mv j2objc-${j2objc_version} Distributive
-rm j2objc-${j2objc_version}.zip
+curl -OL http://cocoas.oss-cn-hangzhou.aliyuncs.com/${zipfile}.zip
+
+# echo "${sha1_checksum}  j2objc-${j2objc_version}.zip" | shasum -c
+
+unzip -o -q ${zipfile}.zip
+mv ${zipfile} Distributive
 
 echo "Creating framework"
+mkdir Cache
+mv ${zipfile}.zip Cache/${zipfile}.zip
+
 mkdir Frameworks
 mkdir Frameworks/j2objc.framework
 cp -a Scripts/Template/* Frameworks/j2objc.framework/
